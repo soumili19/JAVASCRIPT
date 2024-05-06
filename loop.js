51630f97b23fc713fe63411fe96f337741d3acec
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-05-06 21:23:28 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,43 @@
+let sum=0;
+for(let i=1;i<=5;i++)
+{
+  sum=sum+i;
+}
+console.log("Sum = ",sum);
+console.log("loop has ended");
+for(let i=0;i<=5;i++)
+{
+  console.log("i = ",i)
+}
+//for(let i=1;i>=0;i++)//infinte loop//
+//{
+ // console.log("i =",i);
+//}
+//while(i<=5)//do loop//
+//{
+//  console.log("i =",i);
+ // i++;
+//}
+//do{
+ // console.log("i=",i);//do while loop//
+  //i++;
+//}while(i<=5);
+//let str="javascript"//for-of-loop//
+//for(let i of strVar)
+//{
+  //console.log("i=",i);
+  //size++;
+//}
+//console.log("string size =",size);//for-in-loop//
+let student={
+  name:"Soumili Dey",
+  age:20,
+  cgpa:7.5,
+  isPass:true,
+};
+//for(let key in student){
+ // console.log("i =",i);
+  //console.log("key =",key,"value",student[key]);
+//}
+
+
