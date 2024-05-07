1dd862cd56ed200a0a0a955c0e1c283da2f73420
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-05-07 11:17:34 +0530

    Update loop_pracrice.js

--- loop_pracrice.js
+++ loop_pracrice.js
@@ -1,13 +1,13 @@
-//print all even number from 0 to 100//
-//for(let i =0;i<=100;i++)
-//{
-   // if(i%2==0)
-    //console.log(i)
-//}
+print all even number from 0 to 100//
+for(let i =0;i<=100;i++)
+{
+    if(i%2==0)
+    console.log(i)
+}
 //guess number game//
-//let gameNum=25;
-//let userNum=promt("Guess the game number : ");
-//while(gamenum!=userNum){
- //   userNum=prompt("You enterde wrong number!! guess the right number");
-//}
-//console.log("cingrtulations,you enterd right number");
+let gameNum=25;
+let userNum=promt("Guess the game number : ");
+while(gamenum!=userNum){
+    userNum=prompt("You enterde wrong number!! guess the right number");
+}
+console.log("cingrtulations,you enterd right number");
