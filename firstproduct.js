6e77f85f58458767886d8518d799ad850e06ead5
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-05-09 21:36:50 +0530

    Add files via upload

---
+++ firstproduct.js
@@ -0,0 +1,14 @@
+const product={
+    title:"Ballpen",
+    ratings:4,
+    offer:5,
+    price:270,
+};
+console.log(product);
+const profile={
+    userName:"shradha khapra",
+    Followers:569,
+    Following:4,
+    isFollow:true,
+}
+console.log(profile)
