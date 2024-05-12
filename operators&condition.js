9aa4a379506f45ddc1e9db881ab257580ad15dac
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-05-12 10:14:59 +0530

    Add files via upload

---
+++ operators&condition.js
@@ -0,0 +1,23 @@
+/*let num =prompt("Enter a number");
+if(num%5===0){
+    console.log(num,"multiple is 5");
+}
+else{
+    console.log(num,"multiple is not 5");
+}*/
+let num = prompt("Enter your score");
+if(num>=80&&num<=100){
+    console.log(num,"Grade is A");
+}
+else if(num>=70&&num<=89){
+    console.log(num,"Grade is B");
+}
+else if(num>=60&&num<=69){
+    console.log(num,"Grade is C");
+}
+else if(num>=50&&num<=59){
+    console.log(num,"Grade is D");
+}
+else{
+    console.log(num,"Grade is F");
+}
