0800ae1d3e47dff4be1c7f149acbe6ecef4c4a9c
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-05-17 23:22:34 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,22 @@
+let str="Apna College";
+let str2="Soumili";
+console.log(str[1]);
+//Template Litreals//
+let specialString=`This is a template literal`;
+let obj={
+    item:"pen",
+    price:10,
+};
+console.log(specialString);
+let output=`the cost of ${obj.item}is${obj.price}rupees`;
+console.log(output);
+console.log("Apna\nCollege");
+console.log("Apna\tCollege");
+let str3="Apna college";
+let newStr=str.toUpperCase();
+console.log(str3);
+console.log(newStr);
+let str4="hello";
+console.log()
+
+
